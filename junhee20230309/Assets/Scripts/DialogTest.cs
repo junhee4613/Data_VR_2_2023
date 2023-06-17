df6cf2f50a286e109bdbb5315ac8fc943e105b2f
junhee4613 <junhee000123@naver.com>
2023-06-17 16:24:55 +0900

    주석 추가

--- junhee20230309/Assets/Scripts/DialogTest.cs
+++ junhee20230309/Assets/Scripts/DialogTest.cs
@@ -12,4 +12,5 @@ public class DialogTest : MonoBehaviour
         yield return new WaitUntil(() => dialogSystem.UpdateDialog(0, true));   //기다리는 함수 , 다이얼로그 시스템이 완료 될 때까지
         //인수는 대사 번호
     }
+
 }
